9cb5c95821af8911377465f5c1495a814b62f9ed
Skander Gasmi <skandergasmi7@gmail.com>
2023-10-15 15:12:06 +0100

    update

--- 2 recurrent neurals networks for language modeling/utils.py
+++ 2 recurrent neurals networks for language modeling/utils.py
@@ -1,4 +1,15 @@
 import numpy as np
+import trax
+from trax import layers as tl
+
 
 def sigmoid(x): 
     return 1.0 / (1.0 + np.exp(-x))
+
+
+
+def show_layers(model, layer_prefix="Serial.sublayers"):
+    print(f"Total layers: {len(model.sublayers)}\n")
+    for i in range(len(model.sublayers)):
+        print('========')
+        print(f'{layer_prefix}_{i}: {model.sublayers[i]}\n')
